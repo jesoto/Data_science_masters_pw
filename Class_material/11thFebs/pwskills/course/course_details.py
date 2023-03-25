b2c5991d368ce0f287e432bf3f4a966e101efdfe
Jesús Soto <jesus.soto@pucp.edu.pe>
2023-03-25 16:02:14 -0500

    Create course_details.py

---
+++ Class_material/11thFebs/pwskills/course/course_details.py
@@ -0,0 +1,15 @@
+import os,sys
+from os.path import dirname, join, abspath
+
+sys.path.insert(0, abspath(join(dirname(__file__),'..')))
+
+from payment import payment_details
+
+
+
+
+def course():
+    print("my course file")
+    
+
+payment_details.payment()
