8f78c66e7e08e1044cc51970882bf623bb43dd2c
Jesús Soto <jesus.soto@pucp.edu.pe>
2023-04-02 15:15:07 -0500

    Update app.py

--- Class_material/23thFebs/app.py
+++ Class_material/23thFebs/app.py
@@ -2,11 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
-
 def hello_world():
-    return "Hello, World"
+    return "<h1>Hello, World!</h1>"
 
-if __name__ == "__main__":
+
+@app.route("/hello1")
+def hello_world1():
+    return "<h1>Hello, World1!</h1>"
+
+@app.route("/hello2")
+def hello_world2():
+    return "<h1>Hello, World2!</h1>"
+
+@app.route("/test_fun")
+def test():
+    a = 5+6
+    return "this my first fun in flask {}".format(a)
+
+if __name__=="__main__":
     app.run(host="0.0.0.0")
